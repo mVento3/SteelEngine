3ec30de1d09220770dfaa7d68a88f8713c511054
MisterVento3 <forlolbolo@wp.pl>
2020-01-20 18:57:15 +0100

    Added tpis feature to console

--- include/ImGUI_Editor/EditorWindows/ConsoleWindow.h
+++ include/ImGUI_Editor/EditorWindows/ConsoleWindow.h
@@ -9,6 +9,12 @@
 
 #include "Logger/LogDispatcher.h"
 
+#include "imgui/imgui.h"
+
+#include "ConsoleSystem/IConsoleSystem.h"
+
+#include "Event/EventObserver.h"
+
 #include "ImGUI_Editor/EditorWindows/ConsoleWindow.Generated.h"
 
 namespace SteelEngine {
@@ -18,13 +24,22 @@ namespace SteelEngine {
         SteelEngine::Editor::ReflectionAttributes::EDITOR_WINDOW,
         SteelEngine::ReflectionAttribute::GENERATE_CAST_FUNCTIONS,
         SteelEngine::EditorComponents::ImGUI::UserInterface::Attributes::SEPARATE_WINDOW,
+        SteelEngine::EditorComponents::ImGUI::UserInterface::Attributes::FLAGS = ImGuiWindowFlags_::ImGuiWindowFlags_NoScrollbar,
         SteelEngine::ReflectionAttribute::RUNTIME_SERIALIZE
     )
-    class ConsoleWindow : public EditorComponents::ImGUI::UserInterface, public LogDispatcher
+    class ConsoleWindow : public EditorComponents::ImGUI::UserInterface, public LogDispatcher, public EventObserver
     {
         GENERATED_BODY
     private:
         std::vector<LogData> m_Logs;
+
+        bool m_ScrollDown;
+        char* m_CommandBuffer;
+        const Type::uint32 m_CommandBufferSize = 1024;
+
+        bool m_Keys[256] = { false };
+
+        IConsoleSystem** m_ConsoleSystem;
 
     public:
         ConsoleWindow();
@@ -34,6 +49,7 @@ namespace SteelEngine {
         void Draw() override;
 
         void Dispatch(LogData log) override;
+        void OnEvent(Event::Naive* event) override;
 
         void OnRecompile(HotReloader::IRuntimeObject* oldObject) override;
     };
