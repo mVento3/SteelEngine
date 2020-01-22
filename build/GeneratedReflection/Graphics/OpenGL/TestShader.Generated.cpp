818effefa980dcc00c361f28daec49b77002e93c
MisterVento3 <forlolbolo@wp.pl>
2020-01-22 17:48:05 +0100

    Updated generated reflection files and config

---
+++ build/GeneratedReflection/Graphics/OpenGL/TestShader.Generated.cpp
@@ -0,0 +1,5 @@
+#include "d:\Projects\C++\SteelEngine/build/GeneratedReflection/Graphics/OpenGL/TestShader.Generated.h"
+#include "Graphics/OpenGL/TestShader.h"
+#include "HotReloader/IRuntimeObject.h"
+#include "RuntimeReflection/ReflectionRecorder.h"
+
---
+++ build/GeneratedReflection/Math/Camera.Generated.cpp
@@ -0,0 +1,5 @@
+#include "d:\Projects\C++\SteelEngine/build/GeneratedReflection/Math/Camera.Generated.h"
+#include "Math/Camera.h"
+#include "HotReloader/IRuntimeObject.h"
+#include "RuntimeReflection/ReflectionRecorder.h"
+
