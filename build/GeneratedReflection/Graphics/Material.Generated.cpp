7be4f16706d3afc9aff70ca25ee8cc1f33349a0b
MisterVento3 <forlolbolo@wp.pl>
2020-02-29 15:43:38 +0100

    Updated generated reflection files

---
+++ build/GeneratedReflection/Graphics/Material.Generated.cpp
@@ -0,0 +1,23 @@
+#include "HotReloader/IRuntimeObject.h"
+#include "RuntimeReflection/ReflectionRecorder.h"
+#include "Graphics/Material.h"
+#include "d:\Projects\C++\SteelEngine/build/GeneratedReflection/Graphics/Material.Generated.h"
+
+namespace SteelEngine {
+namespace Graphics {
+REGISTER_REFLECTION
+{
+SteelEngine::ReflectionRecorder::Register<Material>("Material",{
+"SteelEngine",
+"Graphics"
+}
+)
+(
+SteelEngine::Reflection::MetaData("sizeof", sizeof(Material))
+)
+.Constructor<const char*>()
+.Inheritance<IMaterial>("IMaterial")
+;
+}
+}
+}
--- build/GeneratedReflection/ImGUI_Editor/ImGUI_Editor.Generated.cpp
+++ build/GeneratedReflection/ImGUI_Editor/ImGUI_Editor.Generated.cpp
@@ -49,6 +49,7 @@ SERIALIZE(ImGUI_Editor::m_NaiveManager)
 SERIALIZE(ImGUI_Editor::m_MainEditorWindows)
 SERIALIZE(ImGUI_Editor::m_StartMenuWindows)
 SERIALIZE(ImGUI_Editor::m_UIs)
+SERIALIZE(ImGUI_Editor::texture)
 }
 #ifdef RUNTIME_COMPILE
 extern "C" __declspec(dllexport) TypeInfo* allocateRuntimeObject(SteelEngine::RuntimeDatabase::ConstructedObjectsVector* typeInfo)
